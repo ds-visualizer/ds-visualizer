db665292f6fbd68d48c508c3a3217ca160f84402
satvik-1203 <satvikgsu@gmail.com>
2021-10-19 23:07:59 -0400

    :bug: overflow scroll on queue

--- components/algos/queue/index.tsx
+++ components/algos/queue/index.tsx
@@ -48,10 +48,12 @@ const index: React.FC<Props> = ({ html }) => {
           />
         </Buttons>
       </OptionBackground>
-      <div className="flex mt-4 space-x-3 items-center justify-center border-2 border-purple-500 w-min mx-auto px-3 border-l-0 border-r-0 h-20">
-        <AnimateSharedLayout>
-          <AnimatePresence>{queue}</AnimatePresence>
-        </AnimateSharedLayout>
+      <div className="w-screen overflow-scroll">
+        <div className="flex mt-4 space-x-3 items-center justify-center border-2 border-purple-500 w-min mx-auto px-3 border-l-0 border-r-0 h-20">
+          <AnimateSharedLayout>
+            <AnimatePresence>{queue}</AnimatePresence>
+          </AnimateSharedLayout>
+        </div>
       </div>
 
       <div>
