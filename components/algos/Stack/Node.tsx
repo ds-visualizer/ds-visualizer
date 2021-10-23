d469dc57512f6f06415eaff385e51e8f2b01427f
Satvik <satvikgsu@gmail.com>
2021-10-23 08:19:37 -0400

    Update Node.tsx

--- components/algos/Stack/Node.tsx
+++ components/algos/Stack/Node.tsx
@@ -1,28 +1,17 @@
 import { motion } from "framer-motion";
 
 export default class Node {
-<<<<<<< HEAD
-  private readonly value: number;
-  private static count = 0;
-  private countN: number;
 
-  constructor(value: number) {
-=======
   public readonly value: any;
   private static count = 0;
   private countN: number;
 
   constructor(value: any) {
->>>>>>> 314da669bc445dc844f921c1e827f8542b22b19c
     this.value = value;
     this.countN = Node.count++;
   }
 
-<<<<<<< HEAD
-  render() {
-=======
   render(key?: number) {
->>>>>>> 314da669bc445dc844f921c1e827f8542b22b19c
     return (
       <motion.div
         layout
