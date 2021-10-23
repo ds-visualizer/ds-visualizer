3fd19c3d04aa49ad8bc2ba40550a9e8160e6982f
satvik-1203 <satvikgsu@gmail.com>
2021-10-23 08:55:03 -0400

    graph

---
+++ components/algos/graph/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const index: React.FC<Props> = () => {
+  return <div className=""></div>;
+};
+
+export default index;
---
+++ pages/graph.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import type { NextPage } from "next";
+
+import Graph from "@Components/algos/graph";
+
+const graph: NextPage = () => {
+  return (
+    <>
+      <Graph />
+    </>
+  );
+};
+
+export default graph;
