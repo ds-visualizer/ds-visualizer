b9b1ba4906975ab1d802a20c8e8292320dfba2c6
satvik-1203 <satvikgsu@gmail.com>
2021-10-23 22:55:45 -0400

    added title

--- pages/graph.tsx
+++ pages/graph.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import type { NextPage } from "next";
 
 import Graph from "@Components/algos/graph";
+import Head from "next/head";
 
 const graph: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Graph</title>
+      </Head>
       <Graph />
     </>
   );
