d66bc2eee233469bcf6a0c62df9082d8460b29af
shah1012 <69828183+shah1012@users.noreply.github.com>
2021-10-24 21:45:41 -0500

    commit 3

---
+++ pages/SelectionSort.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+import SelectSort from "../components/algos/SelectionSort";
+
+const SelectionSort = () => {
+  return (
+    <>
+      <Head>
+        <title>Selection Sort</title>
+      </Head>
+      <SelectSort />
+    </>
+  );
+};
+
+export default SelectionSort;
