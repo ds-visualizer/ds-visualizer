b82c420e7aa6cbdaa26ac4143d3abce23f28f527
satvik-1203 <satvikgsu@gmail.com>
2021-10-26 12:47:24 -0400

    changed home meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import Metadata from "@Root/components/layouts/Metadata";
 import Home from "@Root/components/pages/Home";
+import { InferGetStaticPropsType } from "next";
 
-export default function index() {
+export default function index({
+  metaData,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className="flex flex-col items-center justify-center py-2">
-      <Metadata title="Home" />
+      <Metadata {...metaData} />
       <Home />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const metaData = await import("@Misc/Meta.json");
+  return { props: { metaData: metaData.home } };
+};
