423b0b0770fec4045c6d540885b0b45b1775c9b0
Satvik <satvikgsu@gmail.com>
2021-10-27 00:11:25 -0400

    :sparkles: added tree content (#36)

--- misc/routes.ts
+++ misc/routes.ts
@@ -3,6 +3,7 @@ import Route from "@Root/interface/Route";
 const routes: Array<Route> = [
   { name: "Graph", path: "/graph" },
   { name: "Binary Tree", path: "/binarytree" },
+  { name: "Tree", path: "/tree" },
   { name: "Queue", path: "/queue" },
   { name: "Stack", path: "/stack" },
   { name: "LinkedList", path: "/linkedlist" },
