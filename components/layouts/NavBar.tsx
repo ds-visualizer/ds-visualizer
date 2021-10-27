aa2919421c400a6e9f036a09429c3e103d1040d4
satvik-1203 <satvikgsu@gmail.com>
2021-10-27 00:09:47 -0400

    added tree content

--- components/layouts/NavBar.tsx
+++ components/layouts/NavBar.tsx
@@ -5,8 +5,8 @@ interface Props {}
 
 const NavBar: React.FC<Props> = () => {
   return (
-    <div className=" bg-[#141414] sticky top-0 z-40  ">
-      <div className=" border-b py-3 px-3 mx-8 border-gray-600">
+    <div className=" sticky top-0 z-40  ">
+      <div className=" border-b backdrop-blur-[3px] py-3 px-3 mx-8 border-gray-600">
         <Link href="/">
           <a className="text-white">Home</a>
         </Link>
