e28624bd8e067ba52b58320b799fbd7233dae9a8
Satvik <satvikgsu@gmail.com>
2021-11-03 11:34:32 -0400

    💄  Added code links (#58)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,15 @@ import type { AppProps } from "next/app";
 import "@Root/styles/vs-code.css";
 import "../styles/globals.css";
 import Layout from "@Components/layouts";
+import Metadata from "@Root/components/layouts/Metadata";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <>
+        <Metadata title="Ds-Visualizer" />
+        <Component {...pageProps} />
+      </>
     </Layout>
   );
 }
