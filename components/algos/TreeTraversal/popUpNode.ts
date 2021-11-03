84d373d28b111dbc1d29332e7731145afd87f17a
Satvik <satvikgsu@gmail.com>
2021-11-02 22:34:01 -0400

    :lipstick: Added color change (#54)

--- components/algos/TreeTraversal/popUpNode.ts
+++ components/algos/TreeTraversal/popUpNode.ts
@@ -8,8 +8,10 @@ const popUp = async (node: Node<any> | null) => {
   const nodeElementNode = nodeElement.querySelector<HTMLDivElement>(".node");
   if (!nodeElementNode) return;
   nodeElementNode.style.transform = "scale(1.3)";
+  nodeElementNode.style.backgroundColor = "#be12c9";
   await timeOut(1000);
   nodeElementNode.style.transform = "scale(1)";
+  nodeElementNode.style.backgroundColor = "#9ca3af";
 };
 
 export default popUp;
