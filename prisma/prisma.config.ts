d823fd2acc2455054667f8c67f2f846ddca2cbee
satvik-1203 <satvikgsu@gmail.com>
2021-11-18 14:15:42 -0500

    prisma Change

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -13,6 +13,16 @@ import { PrismaClient } from "@prisma/client";
 
 //   prisma = global.prisma;
 // }
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
+
+declare global {
+  var prisma: PrismaClient;
+}
+
+export const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
 
 export default prisma;
