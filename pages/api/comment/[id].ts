669780e187af6fe7276d54ae41a1405ee4697642
Satvik <satvikgsu@gmail.com>
2021-11-21 08:43:38 -0500

    📦 Added jwt package (#87)

--- pages/api/comment/[id].ts
+++ pages/api/comment/[id].ts
@@ -5,6 +5,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method === "DELETE") {
+  }
+
   if (req.method !== "GET") res.status(405).end("Method not allowed");
 
   const { id } = req.query;
