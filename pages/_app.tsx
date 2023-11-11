b85912295eeca474487008ba47536b7e96f26d4e
satvik-1203 <satvikgsu@gmail.com>
2023-11-11 13:19:21 -0500

    feat(package) added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import "@Root/styles/vs-code.css";
 import "../styles/globals.css";
 import Layout from "@Components/layouts";
 import GlobalStore from "@Context/store";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <GlobalStore>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </GlobalStore>
   );
